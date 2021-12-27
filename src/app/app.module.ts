55a11cabb57116554a3c8aa74a190b62e72abae6
gbtec-ext-alexandrepazos <alexandre.pazos@gbtec.es>
2021-12-27 01:41:28 +0100

    Menu - size and basic form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListEntryComponent } from './todo-list-entry/todo-list-entry.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,12 @@ import { TodoListEntryComponent } from './todo-list-entry/todo-list-entry.compon
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
